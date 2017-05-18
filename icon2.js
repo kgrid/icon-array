688d8ff6cd83e4cb4c8dca11c931b2c152d4c6e3
Andrew Cullen <drewcullen@Andrews-MacBook-Pro.local>
2017-05-18 17:27:09 -0400

    seems to be working with class change, going to test

--- icon2.js
+++ icon2.js
@@ -1,73 +1,27 @@
 //divID , count, gridWidth = 10, gridHeight = 10, personFill = "steelblue", backgroundFill = "#FFFFFF", key=true
 
-/**
- * draws icon array visual by appending svg tags
- * @param  {Object} instr obect containing options for icon array
- * instr should contain the following keys
- * {
- *      divID [String]: the ID of the HTML tag under which the array will be drawn,
- *      count [int|float]: the number of icons that will be filled (this can be an integer or a float),
- *      gridWidth [int]: the number of icons spanning horizontally (defaults to 10),
- *      gridHeight [int]: the number of icons spanning vertivally (defaults to 10),
- *      personFill [String]: the color that the icons will be filled with (default is steelblue),
- *      backgroundFill [String]: background color of the web page (defaults to "#FFFFFF"),
- *      key [boolean]: set to true to show key, false to hide (defaults to true)
- * }
- */
+
 var IconArray = (function () {
     "use strict";
-    //for drawing svg icon path
-    var path = "M0,0v40h22V0H0z M11,0.732c1.755,0,3.177,1.423,3.177,3.177c0,1.755-1.422,3.177-3.177,3.177" +
-        "c-1.754,0-3.177-1.422-3.177-3.177C7.823,2.155,9.246,0.732,11,0.732z M18.359,11.884v9.851c0,0.763-0.617,1.381-1.381,1.381" +
-        "c-0.763,0-1.381-0.618-1.381-1.381v-8.967h-0.535v0.124v10.224v14.307c0,1.02-0.826,1.848-1.848,1.848" +
-        "c-1.02,0-1.846-0.828-1.846-1.848V23.114h-0.697v14.307H10.63c0,1.021-0.827,1.847-1.847,1.847c-1.021,0-1.847-0.826-1.847-1.847" +
-        "c0-0.134,0.016-0.264,0.043-0.39V23.114H6.937V12.767H6.401v8.967c0,0.763-0.618,1.381-1.381,1.381s-1.38-0.619-1.38-1.383v-9.85" +
-        "v-0.407c0-2.032,1.647-3.679,3.68-3.68h7.362c2.03,0,3.68,1.647,3.68,3.68v0.408H18.359z";
-
-    var personHeight = 39;
-    var personWidth = 19;
-    var defaultHeight = 10
-    var defaultWidth = 10
-    var defaultFill = "steelblue"
-
-    //-----FUNCTIONS-----
-    var initialize_svg = function(divID, width, height, backgroundFill) 
-    {
-        var svgContainer = d3.selectAll("#" + divID).append("svg")
-            .attr("class", "icon-array")
-            .attr("fill", backgroundFill)
-            .attr("width", width)
-            .attr("height", height);
-
-        return svgContainer;
-    }
-
-    var draw_person = function(svgContainer, fill, path, x, y, backgroundFill, type="icon-body") 
-    {
-        svgContainer.append("rect")
-            .attr("class", type)
-            .attr("height", personHeight)
-            .attr("width", personWidth)
-            .attr("fill", fill)
-            .attr("transform", "translate(" + x + ", " + y + ")");
-
-        svgContainer.append("path")
-            .attr("fill", backgroundFill)
-            .attr("d", path)
-            .attr("transform", "translate(" + x + ", " + y + ")");
-    }
 
     //Artist class
     var Artist = (function()
     {
-        //---PRIVATE METHODS---
         //---PUBLIC METHODS---
 
         //constructor
-        function Artist(in_fill, in_backgroundFill) 
+        /**
+         * Artist constructor
+         * 
+         * @param {string} in_fill fill color for icons
+         * @param {string} in_backgroundFill background color of web page
+         * @param {object} in_svg svg element that will be used to draw icons
+         */
+        function Artist(in_fill, in_backgroundFill, in_svg) 
         {
             this.iconFill = in_fill
             this.backgroundFill = in_backgroundFill
+            this.svgContainer = in_svg
 
             this.path = "M0,0v40h22V0H0z M11,0.732c1.755,0,3.177,1.423,3.177,3.177c0"
                 + ",1.755-1.422,3.177-3.177,3.177c-1.754,0-3.177-1.422-3.177-3.177C7.82"
@@ -82,21 +36,16 @@ var IconArray = (function () {
 
             this.personHeight = 39;
             this.personWidth = 19;
-            this.defaultHeight = 10
-            this.defaultWidth = 10
-            this.defaultFill = "steelblue"
-
-            return Artist
         }
 
         //draw person
-        Artist.prototype.draw_person = function(x, y, type = "icon-body") 
+        Artist.prototype.draw_person = function(x, y, filledIn, type="icon-body") 
         {
             this.svgContainer.append("rect")
                 .attr("class", type)
                 .attr("height", this.personHeight)
                 .attr("width", this.personWidth)
-                .attr("fill", this.personFill)
+                .attr("fill", filledIn ? this.iconFill : "#cccccc")
                 .attr("transform", "translate(" + x + ", " + y + ")");
 
             this.svgContainer.append("path")
@@ -109,14 +58,14 @@ var IconArray = (function () {
         Artist.prototype.draw_partial_person = function(x, y, portion,
             type = "partial-icon-body") 
         {
-            svgContainer.append("rect")
+            this.svgContainer.append("rect")
                 .attr("class", type + "-bottom")
                 .attr("height", this.personHeight)
                 .attr("width", this.personWidth)
-                .attr("fill", this.personFill)
+                .attr("fill", this.iconFill)
                 .attr("transform", "translate(" + x + ", " + y + ")");
 
-            svgContainer.append("rect")
+            this.svgContainer.append("rect")
                 .attr("class", type + "-top")
                 .attr("height", this.personHeight * (1 - portion))
                 .attr("width", this.personWidth)
@@ -124,39 +73,44 @@ var IconArray = (function () {
                 .attr("transform", "translate(" + x + ", " + y + ")");
 
 
-            svgContainer.append("path")
+            this.svgContainer.append("path")
                 .attr("fill", this.backgroundFill)
                 .attr("d", this.path)
                 .attr("transform", "translate(" + x + ", " + y + ")");
         }
+
+        return Artist
     })();
-   
 
-    var draw_partial_person = function(svgContainer, fill, path, x, y, portion, 
-                                backgroundFill, type="partial-icon-body") 
+    //-----FUNCTIONS-----
+
+    /**
+     * creates svg element within specified div and initliazes it to specified
+     * dimensions
+     * 
+     * @param {string} divID ID of div containing icon array
+     * @param {number} width width of svg canvas
+     * @param {number} height height of svg canvas
+     * @param {string} backgroundFill background color for svg canvas
+     * @returns {object} svg element
+     */
+    var initialize_svg = function(divID, width, height, backgroundFill) 
     {
-
-        svgContainer.append("rect")
-            .attr("class", type + "-bottom")
-            .attr("height", personHeight)
-            .attr("width", personWidth)
-            .attr("fill", fill)
-            .attr("transform", "translate(" + x + ", " + y + ")");
-
-        svgContainer.append("rect")
-            .attr("class", type + "-top")
-            .attr("height", personHeight * (1 - portion))
-            .attr("width", personWidth)
-            .attr("fill", "#cccccc")
-            .attr("transform", "translate(" + x + ", " + y + ")");
-
-
-        svgContainer.append("path")
+        var svgContainer = d3.selectAll("#" + divID).append("svg")
+            .attr("class", "icon-array")
             .attr("fill", backgroundFill)
-            .attr("d", path)
-            .attr("transform", "translate(" + x + ", " + y + ")");
-    }
+            .attr("width", width)
+            .attr("height", height);
 
+        return svgContainer;
+    }
+   
+    /**
+     * returns true if number has a decimal, false otherwise
+     * 
+     * @param {number} num number
+     * @returns {bool}
+     */
     var hasDecimal = function(num)
     {
         return (num - Math.floor(num) > 0)
@@ -202,20 +156,20 @@ var IconArray = (function () {
      * @param {string} fillColor color to fill the icons when updating
      * @return {number} the number of icons that are filled after the update
      */
-    var update_array = function(divID, totalIcons, numCurrentFilled, newCount,
-                        fillColor)
+    var update_array = function(artist, divID, totalIcons, numCurrentFilled, newCount)
     {
         var decimalExists = hasDecimal(newCount)
+        //figure out number of svg rectangles that will need to be drawn
         var numToFill = Math.floor(newCount)
         if(numToFill < numCurrentFilled)
         {
             //recolor all the ones after to be grey
             $("#" + divID + " .icon-body").attr("fill", "#cccccc")
         }
-
+        //remove any partially filled icons
         remove_partial(divID)
         //fill the icons
-        $("#" + divID + " .icon-body").slice(-1 * numToFill).attr("fill", fillColor)
+        $("#" + divID + " .icon-body").slice(-1 * numToFill).attr("fill", artist.iconFill)
 
         if(decimalExists)
         {
@@ -225,8 +179,11 @@ var IconArray = (function () {
             var yPos = positions[1]
 
             //fill a partial person icon in the position
-            draw_partial_person(d3.select("#" + divID + " svg"), fillColor, path, xPos,
-                yPos, newCount - Math.floor(newCount), "white")
+
+            artist.draw_partial_person(xPos, yPos, newCount - Math.floor(newCount))
+
+            // draw_partial_person(d3.select("#" + divID + " svg"), fillColor, path, xPos,
+            //     yPos, newCount - Math.floor(newCount), "white")
             console.log("WIDTH AND HEIGHT: ", xPos, yPos)
 
             numToFill += 2
@@ -235,41 +192,62 @@ var IconArray = (function () {
         return numToFill
     }
 
+    //default values
+    var defaultHeight = 10
+    var defaultWidth = 10
+    var defaultFill = "steelblue"
+    var defaultBackgroundFill = "#ffffff"
+    var personHeight = 39;
+
+
     //-----BEGIN-----
     return {
-        draw_array: function(instr) 
+        /**
+         * draws icon array visual by appending svg tags
+         * @param  {Object} instr obect containing options for icon array
+         * instr should contain the following keys
+         * {
+         *      divID [String]: the ID of the HTML tag under which the array will be drawn,
+         *      count [int|float]: the number of icons that will be filled (this can be an integer or a float),
+         *      gridWidth [int]: the number of icons spanning horizontally (defaults to 10),
+         *      gridHeight [int]: the number of icons spanning vertivally (defaults to 10),
+         *      personFill [String]: the color that the icons will be filled with (default is steelblue),
+         *      backgroundFill [String]: background color of the web page (defaults to "#FFFFFF"),
+         *      key [boolean]: set to true to show key, false to hide (defaults to true)
+         * }
+         */
+        draw_array: function (instr) 
         {
             var h = instr.gridHeight ? instr.gridHeight : defaultHeight
             var w = instr.gridWidth ? instr.gridWidth : defaultWidth
             var height = instr.gridHeight ? (45 * instr.gridHeight + 29) : (45 * defaultHeight + 29)
-            console.log(height)
             var width = instr.gridWidth ? (44 * instr.gridWidth) : (44 * defaultWidth + 29)
-            console.log(width)
             var key = instr.key ? true : false
-            console.log(key)
             var fill = instr.personFill ? instr.personFill : defaultFill
-            console.log(fill)
-            var backgroundFill = instr.backgroundFill ? instr.backgroundFill : "#ffffff"
-            console.log(backgroundFill)
-
+            var backgroundFill = instr.backgroundFill ? instr.backgroundFill : defaultBackgroundFill
             //check if there should be a partial fill
             var decimal = instr.count - Math.floor(instr.count);
             if (decimal && decimal < 0.1) 
             {
                 decimal = 0.1
             }
-
             var partial = (decimal != 0) ? true : false;
-            //Make an SVG Container
+
+            //Make an SVG Container with proper dimensions
             var svgContainer = initialize_svg(instr.divID, width, height, backgroundFill);
 
+            //make an object that will be used to draw the svg icons
+            var artist = new Artist(fill, backgroundFill, svgContainer)
+
+            //starting coordinates for beginning of icon array
+            //starts at top left of svg canvas
             var xDist = 25;
             var yCoord = 0;
             var yDist = 45
             var xCoordMultiplier = 0;
-
+            /*figure out how many grey icons ther will be before icons start
+              being filled*/
             var numGrey = (h * w) - Math.ceil(instr.count);
-            console.log(numGrey);
 
             var c = 1;
 
@@ -279,13 +257,13 @@ var IconArray = (function () {
                 {
                     if (c > numGrey && partial) 
                     {
-                        draw_partial_person(svgContainer, (c <= numGrey ? "#cccccc" : fill),
-                            path, xCoordMultiplier * xDist, yCoord, decimal, backgroundFill);
+                        artist.draw_partial_person(xCoordMultiplier * xDist, 
+                                                    yCoord, decimal)
                         partial = false;
                     }
                     else
-                        draw_person(svgContainer, (c <= numGrey ? "#cccccc" : fill),
-                            path, xCoordMultiplier * xDist, yCoord, backgroundFill)
+                        artist.draw_person(xCoordMultiplier * xDist, yCoord, 
+                                            c > numGrey)
                 }
                 xCoordMultiplier = 0;
                 yCoord += yDist;
@@ -293,11 +271,10 @@ var IconArray = (function () {
             if (key && h > 3) 
             {
                 yCoord += 25;
-
                 var txt = "Number of people affected: " + instr.count;
 
-                draw_person(svgContainer, "#cccccc", path, w * xDist + 30, 
-                    (h / 2 * personHeight) - 44, backgroundFill, "key");
+                artist.draw_person(w * xDist + 30, (h / 2 * personHeight) - 44,
+                                    false, "key")
 
                 svgContainer.append("text")
                     .attr("x", w * xDist + 32)
@@ -305,8 +282,8 @@ var IconArray = (function () {
                     .attr("fill", "black")
                     .text("Not affected");
 
-                draw_person(svgContainer, fill, path, w * xDist + 30, 
-                    h / 2 * personHeight + 44, backgroundFill, "key")
+                artist.draw_person(w * xDist + 30, h / 2 * personHeight + 44, 
+                                    true, "key")
 
                 svgContainer.append("text")
                     .attr("x", w * xDist + 32)
@@ -316,7 +293,16 @@ var IconArray = (function () {
             }
         },
 
-        gif_array: function(divID, interval, data, options)
+        /**
+         * Draws an icon array in specified div and makes animation by continuously
+         * updating the array to reflect new values
+         * 
+         * @param {string} divID id of div containing the icon array
+         * @param {number} interval number of seconds between each update
+         * @param {array} data list of numbers that represent different counts
+         * @param {object} options options for icon array
+         */
+        repeat_array: function(divID, interval, data, options)
         {
             var index = 0
             var iconOptions = clone_object(options)
@@ -324,9 +310,11 @@ var IconArray = (function () {
             iconOptions.count = data[index]
             IconArray.draw_array(iconOptions)
             var fill = options.personFill ? options.personFill : defaultFill
+            var backgroundFill = options.backgroundFill ? optiosn.backgroundFill : defaultBackgroundFill
             //total numer of svg rectangles
             var totalIcons = $("#" + divID + " .icon-body").length
-        
+            var artist = new Artist(fill, backgroundFill, d3.select("#" + divID + " svg"))
+
             index += 1   
             var numIconsCurrentlyFilled = data[0]
 
@@ -337,60 +325,15 @@ var IconArray = (function () {
                     index = 0
                 }
                // console.log("index::: ", index)
-               numIconsCurrentlyFilled =  update_array(divID, totalIcons,
-                    numIconsCurrentlyFilled, data[index], fill)
+
+               numIconsCurrentlyFilled = update_array(artist, divID, totalIcons, 
+                                            numIconsCurrentlyFilled, data[index])
+            //    numIconsCurrentlyFilled =  update_array(divID, totalIcons,
+            //         numIconsCurrentlyFilled, data[index], fill)
                 index += 1
 
             }, interval * 1000)         
-        },
-
-        repeat_array: function (divID, interval, data, options)
-        {
-            var index = 0
-            var iconOptions = clone_object(options)
-            iconOptions.divID = divID
-            iconOptions.count = data[index]
-            IconArray.draw_array(iconOptions)
-            index += 1
-            setInterval(function () 
-            {
-                //set index back to beginning of data array if it gets past the end
-                console.log("index:", index)
-                if (index === data.length)
-                {
-                    index = 0
-                }
-                //move on to next data in the list
-                iconOptions.count = data[index]
-                //clear the html
-                //right now using vanilla js, going to change when this no 
-                //  longer uses d3js
-                document.getElementById(divID).innerHTML = ""
-                IconArray.draw_array(iconOptions)
-                index += 1
-            }, interval * 1000)
         }
     }
-
-})();
-
-
-
-/** GIF-like functionality for icon array. Cycles through a list of 
- *  data and changes the visual
- * @param {String} divID ID of the div you want the visual to show up in
- * @param {Number} interval number of seconds in between each icon array update
- * @param {array} data  array of numbers representing different counts
- * @param {object} options options for the icon array
- * example:
- * calling repeat_array("id2", 2, [1, 5, 7.25, 10.5, 23.2], {key: true}) will
- * make a gif-like icon array visual that cycles through the counts 1, 5, 7.25...
- * every 2 seconds and draws to a div with id of "id2". the {key: true} options 
- * object makes the key show up in the visual
-)
- */
-var repeat_array = (function () {
-
-    return
 
 })();
